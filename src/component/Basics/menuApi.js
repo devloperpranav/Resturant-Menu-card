26767a0e6b134e68c07a6a2c23dc5f7cb3cd05f7
PRANAV POHOKAR <113296136+devloperpranav@users.noreply.github.com>
2023-04-23 10:20:48 +0530

    Add files via upload

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,86 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "Maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "MAGGI’s delicious taste lies in the fine blend of spices and herbs found inside the tastemaker sachet. They are an excellent meal by themselves, and can also be enjoyed with a stir fry, eggs and any other topping of choice. ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "Aloo Pakora",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Aloo pakora are soft melt-in-the-mouth fried potato fritters coated in a delicious, golden, crispy batter. It is also known as potato bajji and has always been one of my favorite tea-time snacks. ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "Corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Fresh sweet corn is a staple vegetable enjoyed all year long, but it peaks during the warm summer months. You get the benefits of a vegetable with the taste of a dessert! ",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "Chole",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "This Chana Masala, also known as Chole Masala, is an authentic North Indian style curry made with white chickpeas, freshly powdered spices, onions, tomatoes and herbs. ",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "Pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "A baked dish originating in Italy, which consists of a flat, round, crispy leavened dough covered with a layer of sauce and a layer of one or more toppings, such as meat, seafood, vegetables, and fruit, which is often topped with a layer of cheese. ",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "A typical non-veg meal contains meat or fish along with other vegetarian dishes - for example, dal or sabzi (vegetable dish). My non-vegetarian thali/meals in this collection include not just delicious meat dishes, but also some mouthwatering vegetarian dishes. ",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Eating the sweet item first enables the flow of digestive secretions,“ says nutritionist Supriyaa Nair. “If you eat sweets at the end of meals, you are slowing down your digestion. ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma chawal or rajma rice is delicious rajma masala (red kidney beans masala) mixed with rice, a comfort food for many who grew up eating this great combination of carbs and protein. ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "Samaso",
+      category: "evening",
+      price: "10₹",
+      description:
+        "Samosa, a crispy and spicy deep fried snack that has an crisp and flaky outer layer made of maida and rich filling of mashed potato, peas and spices is a popular street food of India. ",
+    },
+  ];
+  
+  export default Menu;
